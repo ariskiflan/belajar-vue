11985dda9be75811c22a2271dc8c091a2f5cf96d
ariskiflan <ariskiflan98@gmail.com>
2024-06-13 21:10:42 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>Hello {{ name }}</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "aris",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
